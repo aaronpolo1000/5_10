dc96ac228977245e3fab3f13c444cbc699342c71
aaronpolo1000 <aaronpolo1000@gmail.com>
2023-03-10 14:55:20 -0300

    oshiro puto

---
+++ polo/ej01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej04
+{
+    internal class Program
+    {
+        static Int32 suma(Int16 num1, Int16 num2)
+        {
+            return num1 + num2;
+        }
+
+        static void Main(string[] args)
+        {
+            Int16 n1 = 10, n2 = 20;
+            Int32 resultado1, resultado2, resultado3;
+          
+            resultado1 = suma(n1, n2);
+            resultado2 = suma(40, 3);
+            resultado3 = suma(1, 2);
+            int [] num = { resultado1, resultado2, resultado3 };
+            int mayor = num[0];
+              for(int i=1;i<3;i++)
+
+            {
+                if (mayor < num[i])
+                {
+                    mayor = num[i];
+                }
+            }         
+
+            Console.WriteLine("El numero mas grande es " +mayor);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ polo/ej02/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo tecla;
+            int row = 10, col = 10;
+
+            Console.SetCursorPosition(col, row);
+            Console.Write("*");
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                tecla = Console.ReadKey();
+                if (tecla.Key == ConsoleKey.UpArrow)
+                {
+                    Console.SetCursorPosition(col, row);
+                    Console.Write(" ");
+                    if (row > 0)
+                        row--;
+                    Console.SetCursorPosition(col, row);
+                    Console.Write("*");
+                }
+                if (tecla.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(col, row);
+                    Console.Write(" ");
+                    if (row < 25)
+                        row++;
+                    Console.SetCursorPosition(col, row);
+                    Console.Write("*");
+                }
+                if (tecla.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.SetCursorPosition(col, row);
+                    Console.Write(" ");
+                    if (col > 0)
+                        col--;
+                    Console.SetCursorPosition(col, row);
+                    Console.Write("*");
+                }
+                if (tecla.Key == ConsoleKey.RightArrow)
+                {
+                    Console.SetCursorPosition(col, row);
+                    Console.Write(" ");
+                    if (col < 79)
+                        col++;
+                    Console.SetCursorPosition(col, row);
+                    Console.Write("*");
+                }
+                if (tecla.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                
+                }
+            Console.CursorVisible = true;
+
+
+        }
+    }
+}
