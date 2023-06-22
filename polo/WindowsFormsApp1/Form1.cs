77b51b2a0c8a0c3d6fa4327923ea55902435ac5a
aaronpolo1000 <aaronpolo1000@gmail.com>
2023-06-22 17:49:07 -0300

    a

---
+++ polo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox2.Text);
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                listBox2.Items.Add(textBox2.Text);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+    }
+}
