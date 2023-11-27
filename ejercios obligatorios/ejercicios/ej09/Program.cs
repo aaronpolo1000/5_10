5451fb26e61a8f260a229e008fe604d860fc4aba
aaronpolo1000 <aaronpolo1000@email.com>
2023-11-27 20:06:49 -0300

    ejer 8 , 9 y 10

---
+++ ejercios obligatorios/ejercicios/ej09/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ej09
+{
+    internal class Program
+    {
+        static Random rd = new Random();
+        static void limpiarDatos(int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("                                                                       ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[] { "Aaron", "Pedro", "Luis", "Carlos", "Miguel", "Alejandro", "Jorge", "José", "Francisco", "Antonio", "María", "Ana", "Laura", "Carolina", "Sofía", "Isabella", "Valentina", "Gabriela", "Paola", "Fernanda" };
+            List<Pelicula> peliculas = new List<Pelicula>() { new Pelicula("Pokemon Mewtwo", 1.28, "Kunihiko Yuyama", 13), new Pelicula("Five Nights at Freddy's: la pelicula", 2.28, "Emma Tammi", 16), new Pelicula("300", 2.58, "Zack Snyder", 18) };
+            Sala sala = new Sala(peliculas[rd.Next(0, peliculas.Count())], rd.Next(1000, 2400), 8, 8);
+            List<Espectador> espectadores = new List<Espectador>();
+            Console.CursorVisible = false;
+
+            for (int i = 0; i < rd.Next(20, 100); i++)
+            {
+                espectadores.Add(new Espectador(nombres[rd.Next(0, nombres.Count())], rd.Next(1, 99), rd.Next(1000, 10000)));
+            }
+
+            Console.Write("Bienvenido a la sala del cine!!");
+
+            Console.SetCursorPosition(0, 3);
+            Console.WriteLine("Asientos disponibles:");
+            for (int i = 0; i < sala.Columnas; i++)
+            {
+                Console.SetCursorPosition(75, i + 5);
+                for (int j = 0; j < sala.Filas; j++)
+                {
+                    Console.SetCursorPosition(j * 3 + 75, i + 5);
+                    Console.Write(sala.Asientos[8 * i + j].Fila + sala.Asientos[8 * i + j].Columna.ToString() + " ");
+                }
+            }
+
+            Console.SetCursorPosition(75, 20);
+            Console.Write("Pelicula: " + sala.Actual.Titulo);
+            Console.SetCursorPosition(75, 21);
+            Console.Write("Duración: " + sala.Actual.Duracion);
+            Console.SetCursorPosition(75, 22);
+            Console.Write("Director: " + sala.Actual.Director);
+            Console.SetCursorPosition(75, 23);
+            Console.Write("Publico: para mayores de " + sala.Actual.EdadMinima + " años");
+
+            bool bandera = false;
+            for (int i = 0; i < espectadores.Count; i++)
+            {
+                limpiarDatos(7, 14);
+                Espectador red = espectadores[rd.Next(0, espectadores.Count())];
+                Console.SetCursorPosition(0, 7);
+                Console.WriteLine("Datos del espectador:");
+                Console.WriteLine("Nombre: " + red.Nombre);
+                Console.WriteLine("Edad: " + red.Edad + " años");
+                Console.WriteLine("Cantidad de dinero: " + red.Dinero + " pesos");
+                Console.WriteLine("Analizando si el espectador puede entrar...");
+                Thread.Sleep(100);
+                if (sala.puedeEntrar(red.Edad, red.Dinero))
+                    Console.WriteLine("El espectador tiene el suficiente dinero y edad para entrar!!");
+                else
+                {
+                    Console.WriteLine("El espectador no puede entrar porque " + sala.razon(red.Edad, red.Dinero));
+                    Thread.Sleep(100);
+                    continue;
+                }
+                Console.SetCursorPosition(0, 13);
+                Console.Write("                                                ");
+                Console.SetCursorPosition(0, 13);
+                Console.WriteLine("Buscando un asiento...");
+                Console.Write("                                                ");
+                Thread.Sleep(100);
+                if (sala.hayAsientos())
+                {
+                    Asiento ahora = sala.ocuparAsiento();
+                    Console.SetCursorPosition(sala.buscarColumna(ahora.Columna) * 3 + 75, ahora.Fila + 4);
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Write("  ");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.SetCursorPosition(0, 14);
+                    Console.Write("El espectador se sento en el asiento " + ahora.Fila + ahora.Columna);
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, 14);
+                    Console.Write("Ya no hay asientos");
+                    bandera = true;
+                    break;
+                }
+                Thread.Sleep(100);
+            }
+            if (!bandera)
+            {
+                Console.SetCursorPosition(0, 15);
+                Console.WriteLine("Ya no hay mas personas que sentar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercios obligatorios/ejercicios/ej09/Sala.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej09
+{
+    internal class Sala
+    {
+        static private Random rd = new Random();
+        private Pelicula actual;
+        private int columnas, filas;
+        private double precio;
+        private List<Asiento> asientos = new List<Asiento>() { };
+        private char[] letras = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        public Sala(Pelicula actual, double precio, int filas, int columnas)
+        {
+            this.actual = actual;
+            this.precio = precio;
+            if (columnas > letras.Count())
+            {
+                columnas = letras.Count();
+            }
+            this.columnas = columnas;
+            this.filas = filas;
+            for (int i = 0; i < columnas; i++)
+            {
+                for (int j = 0; j < filas; j++)
+                {
+                    asientos.Add(new Asiento(letras[i], j + 1));
+                }
+            }
+        }
+
+        public Pelicula Actual
+        {
+            get
+            {
+                return actual;
+            }
+            set
+            {
+                actual = value;
+            }
+        }
+        public double Precio
+        {
+            get
+            {
+                return precio;
+            }
+            set
+            {
+                precio = value;
+            }
+        }
+        public List<Asiento> Asientos
+        {
+            get
+            {
+                return asientos;
+            }
+        }
+        public int Filas
+        {
+            get
+            {
+                return filas;
+            }
+        }
+        public int Columnas
+        {
+            get
+            {
+                return columnas;
+            }
+        }
+
+        public string razon(int edad, int dinero)
+        {
+            if (edad < actual.EdadMinima)
+                return "no tiene la edad suficiente";
+            else
+                return "no tiene suficiente dinero";
+        }
+        public bool puedeEntrar(int edad, int dinero)
+        {
+            if (edad < actual.EdadMinima)
+                return false;
+            else if (dinero < precio)
+                return false;
+            return true;
+        }
+        public Asiento ocuparAsiento()
+        {
+            int a;
+            while (true)
+            {
+                a = rd.Next(0, asientos.Count());
+                if (!asientos[a].Ocupado)
+                {
+                    asientos[a].Ocupado = true;
+                    return asientos[a];
+                }
+            }
+        }
+        public bool hayAsientos()
+        {
+            foreach (Asiento asientos in asientos)
+            {
+                if (!asientos.Ocupado)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int buscarColumna(char col)
+        {
+            int cont = 0;
+            foreach (char columnas in letras)
+            {
+                if (columnas == col)
+                {
+                    return cont;
+                }
+                cont++;
+            }
+            return 0;
+        }
+    }
+}
